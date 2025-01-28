4490e7d94d88fd1a249d5e5313aa9d2f75920187
Yura Shaplavsky <yurikorock777@gmail.com>
2025-01-28 19:33:05 +0200

    add task 3-4-5

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (email === "" || password === "") {
+    return console.log("All form fields must be filled in");
+  }
+
+  const inputData = {};
+
+  new FormData(form).forEach((value, name) => {
+    inputData[name] = value.trim();
+  });
+
+  form.reset();
+
+  return console.log(inputData);
+}
--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
+const changeWidget = document.querySelector(".change-color");
+const colorSpan = document.querySelector(".color");
+const body = document.body;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+changeWidget.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
